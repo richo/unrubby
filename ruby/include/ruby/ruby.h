4d77c3ed537a1b52a4a56d27536dda3d166f531e
Richo Healey <richo@psych0tik.net>
2015-10-20 18:14:57 -0700

    Whitespace

--- ruby/include/ruby/ruby.h
+++ ruby/include/ruby/ruby.h
@@ -652,15 +652,15 @@ struct RFloat {
 struct RString {
     struct RBasic basic;
     union {
-	struct {
-	    long len;
-	    char *ptr;
-	    union {
-		long capa;
-		VALUE shared;
-	    } aux;
-	} heap;
-	char ary[RSTRING_EMBED_LEN_MAX + 1];
+        struct {
+            long len;
+            char *ptr;
+            union {
+                long capa;
+                VALUE shared;
+            } aux;
+        } heap;
+        char ary[RSTRING_EMBED_LEN_MAX + 1];
     } as;
 };
 #define RSTRING_NOEMBED FL_USER1
